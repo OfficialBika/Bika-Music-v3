d1ddfd84e107b9854b5ee4b01785af815fe7a276
Official Bika <phoetaay@gmail.com>
2026-04-30 12:54:23 +0700

    Update config.py

--- config.py
+++ config.py
@@ -12,7 +12,11 @@ class Config:
         self.MONGO_URL = getenv("MONGO_URL")
 
         self.LOGGER_ID = int(getenv("LOGGER_ID", 0))
-        self.OWNER_ID = int(getenv("OWNER_ID", 0))
+        self.OWNER_ID = [
+          int(x.strip())
+          for x in getenv("OWNER_ID", "0").split(",")
+          if x.strip()
+        ]
 
         self.DURATION_LIMIT = int(getenv("DURATION_LIMIT", 120)) * 60
         self.QUEUE_LIMIT = int(getenv("QUEUE_LIMIT", 25))
